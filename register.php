77df65d7d7167596d1f3a3067e25ee697b0f65e8
curseb100d <libyahan123@gmail.com>
2024-10-14 17:08:39 +0800

    Version 1.2.0

--- register.php
+++ register.php
@@ -1,3 +1,5 @@
+<?php include 'register-logic.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,25 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Link to styles.css -->
-    <link rel="stylesheet" href="styles-signup.css">
+    <link rel="stylesheet" href="register-styles.css">
     <title>Sign Up</title>
 </head>
 <body>
-    <forn action="">
+    <forn action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <div class="container">
             <h1>Sign Up</h1>
             <p>Please fill out this form to create an account.</P>
 
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required>
+            <label><b>Username</b></label>
+            <input type="text" name="username" required class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
 
-            <label for="password"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="password" id="password" required>
+            <label><b>Password</b></label>
+            <input type="password" name="password" required class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
 
-            <label for="passwordrepeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="passwordrepeat" id="passwordrepeat" required>
+            <label><b>Confirm Password</b></label>
+            <input type="password" name="confirm_password" required class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
 
-            <button type="submit" class="registerbtn">Register</button>
+            <button type="submit" class="registerbtn" value="Submit">Register</button>
         </div>
 
         <div class="container signin">
